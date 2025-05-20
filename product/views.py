d765755e829362b935b0c62f699401c74d120f10
Alijono24 <azamjonnajottalim@gmail.com>
2025-05-20 09:28:20 +0500

    product app uchun frontent qo'shildi

--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def info(request):
+    products = Product.objects.all()
+
+    context = {
+        'name':"A'zamjon",
+        'fam': "Alijonov",
+        'products':products
+    }
+    return render(request, "product/index.html", context)
