4a85f0cd79e1e6c957f4bc5161a342cd9020be47
Alijono24 <azamjonnajottalim@gmail.com>
2025-05-20 21:10:40 +0500

    a

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Store(models.Model):
     name = models.CharField(max_length=100)
     address = models.TextField()
-    manager = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='managed_stores')
+    manager = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, related_name='managed_stores')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
